c1cbf33c169a77b65b2b271a382234f8d2a40f92
Brunno Ribeiro <brunnorb@gmail.com>
2018-10-17 14:31:05 -0300

    Obtendo um usuario por id

--- src/main/java/com/brunno/workshopmongo/services/UserService.java
+++ src/main/java/com/brunno/workshopmongo/services/UserService.java
@@ -1,22 +1,29 @@
 package com.brunno.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brunno.workshopmongo.domain.User;
 import com.brunno.workshopmongo.repository.UserRepository;
+import com.brunno.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
-		
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
 	}
 
 }
