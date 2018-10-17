ddfe079cabdc9b68e3ce476d975f350d3ca0c30f
Brunno Ribeiro <brunnorb@gmail.com>
2018-10-17 15:19:59 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/brunno/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.brunno.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brunno.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
---
+++ src/main/java/com/brunno/workshopmongo/services/PostService.java
@@ -0,0 +1,40 @@
+package com.brunno.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunno.workshopmongo.domain.Post;
+import com.brunno.workshopmongo.repository.PostRepository;
+import com.brunno.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public Post insert(Post obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	
+	
+
+}
